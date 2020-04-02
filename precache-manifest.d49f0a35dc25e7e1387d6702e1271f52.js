447722c36d826973a859e15ec28c4c0a83d56f66
Shahibuzzaman <shahibuzzaman@outlook.com>
2020-04-02 18:34:40 +0600

    Updates

---
+++ precache-manifest.d49f0a35dc25e7e1387d6702e1271f52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34769bc724ecfdb0c11b7bad41c6688b",
+    "url": "/react-covid19-tracker/index.html"
+  },
+  {
+    "revision": "145d421c67d9e353ce8d",
+    "url": "/react-covid19-tracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "731c6756113f376f5e94",
+    "url": "/react-covid19-tracker/static/css/main.b6f4e3a0.chunk.css"
+  },
+  {
+    "revision": "145d421c67d9e353ce8d",
+    "url": "/react-covid19-tracker/static/js/2.07759ff0.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/react-covid19-tracker/static/js/2.07759ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "731c6756113f376f5e94",
+    "url": "/react-covid19-tracker/static/js/main.d6e14963.chunk.js"
+  },
+  {
+    "revision": "3874a11c6e9f07cf7533",
+    "url": "/react-covid19-tracker/static/js/runtime-main.d0b12f51.js"
+  },
+  {
+    "revision": "c32b990b4db5bd16218fc31af14f7c0f",
+    "url": "/react-covid19-tracker/static/media/prevention.c32b990b.svg"
+  }
+]);
